d6ff643a4201b866406798813e92a419b8d8b87d
GustavoMont <lgmluisgm@gmail.com>
2021-10-21 16:37:38 -0300

    :pencil: create global style

---
+++ src/Components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export default function Header(){
+
+    return (
+        <h1>Hello World.</h1>
+    )
+
+}
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap');
+    body{
+        font-family: 'Poppins', sans-serif;
+    }
+`
+
+export default GlobalStyle;
