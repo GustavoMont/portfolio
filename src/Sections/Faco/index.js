296b391c9eb36ff30e9f97be1648742724d90007
GustavoMont <lgmluisgm@gmail.com>
2022-02-11 10:32:32 -0300

    :pencil: write the content of Faco page

--- src/Sections/Faco/index.js
+++ src/Sections/Faco/index.js
@@ -3,7 +3,35 @@ import Section from "../../styles/Section";
 export default function Faco (){
     return (
         <Section id="o-que-faco" >
-            <h1>Habilidades</h1>
+            <h2>Serviços</h2>
+            <ul>
+                <li>
+                    <div>
+                        <h3>UI Design</h3>
+                        <p>
+                        Faço o design de telas para sites e aplicativos de maneira criativa, fiel ao produto proposto e sempre zelando para que o usuário tenha a melhor experiência possível, tanto de usabilidade quanto no espetáculo visual apresentado.
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Desenvolvimento Front-end</h3>
+                        <p>
+                            Transformo designs em tela reais para seu projeto, seja de uma landing-page, aplicações interas da empresa, blogs, e-commerce e muito mais. Visando sempre a melhor performance; um bom SEO e a melhor experiência para o usuário.  
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Desenvolvimento Backend</h3>
+                        <p>
+                            Desenvolvo API&apos;s para sites e aplicativos para se comunicar com seu banco de dados e poder responder as requisições feitas na sua aplicação. Viso sempre a segurança das informações transmitidas e concilio sempre com a melhor performance. 
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Desenvolvimento Full-stack</h3>
+                        <p>
+                            Construo aplicações completas, aqui junto o desenvolvimento backend e front-end e assim faço tanto a parte visual quanto a part funcional do seu site/aplicativo. 
+                        </p>
+                    </div>
+                </li>
+            </ul>
         </Section>
     )
 }
