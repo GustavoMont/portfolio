a7b44fdf29ff922fddd184f24317a1ee91f7b828
GustavoMont <lgmluisgm@gmail.com>
2022-02-15 07:05:42 -0300

    :pencil: update Apresentation section

--- src/Sections/Apresentation/index.js
+++ src/Sections/Apresentation/index.js
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import { AnimationCard, Card } from '../../styles/Apresentation'
-import Section from '../../styles/Section'
-import Lottie from 'react-lottie-player'
-import devAnimation from './dev-animation.json'
+import React, { useEffect, useState } from "react";
+import { AnimationCard, Card } from "../../styles/Apresentation";
+import Section from "../../styles/Section";
+import Lottie from "react-lottie-player";
+import devAnimation from "./dev-animation.json";
 
 export default function Apresentation() {
-    const [vel, setVel] = useState(1);
-    return (
-        <Section id="home">
-            <Card>
-                <p>Hello, World! Eu sou</p>
-                <h2>Gustavo Monteiro</h2>
-                <p>Desenvolvedor Web</p>
-            </Card>
-            <AnimationCard>
-                <Lottie
-                    loop
-                    play
-                    speed={vel}
-                    animationData={devAnimation}
-                    onMouseEnter={() => setVel(4)} onMouseLeave={() => setVel(1)}
-                    data-barrier
-                />
-            </AnimationCard>
-        </Section>
-    )
-
+  const [vel, setVel] = useState(1);
+  return (
+    <Section id="home">
+      <AnimationCard>
+        <Lottie
+          loop
+          play
+          speed={vel}
+          animationData={devAnimation}
+          onMouseEnter={() => setVel(4)}
+          onMouseLeave={() => setVel(1)}
+          data-barrier
+        />
+      </AnimationCard>
+      <Card>
+        <p>Hello, World! Eu sou</p>
+        <h2>Gustavo Monteiro</h2>
+        <p>Desenvolvedor Web</p>
+      </Card>
+    </Section>
+  );
 }
--- src/styles/Apresentation.js
+++ src/styles/Apresentation.js
@@ -4,7 +4,7 @@ const writte = keyframes`
     to{
         left: 110%;
     }
-`
+`;
 const blink = keyframes`
     0%{
         opacity: 1;
@@ -15,7 +15,7 @@ const blink = keyframes`
     100%{
         opacity: 1;
     }
-`
+`;
 
 // --dark-purple: #36174d;
 // --pink: #ff4778;
@@ -24,67 +24,66 @@ const blink = keyframes`
 // --light-purple: #6f36bc;
 
 export const Card = styled.div`
-    @media(min-width: 768px) {
-        margin: 0;
-    }
-    border-radius: 8px;
-    width: 95vw;
+  @media (min-width: 768px) {
+    margin: 0;
     position: absolute;
     top: 50%;
-    left: 50%;
-    margin: 1rem 0;
-    transform: translate(-50%, -50%);
-    p{
-        font-weight: 100;
-        font-size: .8rem;
-        @media(min-width: 768px) {
-            font-size: 1rem;
-        }
+    transform: translateY(-50%);
+  }
+  margin: 0 auto;
+  p {
+    font-weight: 100;
+    font-size: 0.8rem;
+    @media (min-width: 768px) {
+      font-size: 1rem;
     }
-    h2{
-        font-size: 1.8rem;
-        font-weight: 500;
-        background-color: var(--light-purple) ;
-        padding: .2rem;
-        position: relative;
-        width: min(100%, 450px);
-        overflow: hidden;
-        @media(min-width: 768px) {
-            background-color: transparent;
-            display: inline;
-            font-size: 2rem;
-        }
-        &:after{
-            content: '';
-            display: block;
-            background-color: var(--light-purple);
-            width: 100%;
-            height: 3rem;
-            position: absolute;
-            top: 0;
-            left: 0%;
-            border-left: 3px solid #fff;
-            animation: 4s ${writte} steps(20) alternate-reverse infinite;
-            @media(min-width: 768px) {
-                background-color: transparent;
-                opacity: 0;
-                height: 3rem;
-                animation: 1s ${blink} infinite;
-                top: 2px;
-                left: 100%;
-            }
-        }
+  }
+  h2 {
+    font-size: 1.6rem;
+    font-weight: 500;
+    background-color: var(--light-purple);
+    padding: 0.2rem;
+    position: relative;
+    width: min(100%, 450px);
+    overflow: hidden;
+    @media (min-width: 768px) {
+      background-color: transparent;
+      display: inline;
+      font-size: 2rem;
     }
+    &:after {
+      content: "";
+      display: block;
+      background-color: var(--light-purple);
+      width: 100%;
+      height: 3rem;
+      position: absolute;
+      top: 0;
+      left: 0%;
+      border-left: 3px solid #fff;
+      animation: 4s ${writte} steps(20) alternate-reverse infinite;
+      @media (min-width: 768px) {
+        background-color: transparent;
+        opacity: 0;
+        height: 3rem;
+        animation: 1s ${blink} infinite;
+        top: 2px;
+        left: 100%;
+      }
+    }
+  }
 `;
 
 export const AnimationCard = styled.div`
-    background-color: #fff;
-    transform: scaleX(-1);
-    border-radius: 50px;
-    @media (min-width: 768px) {
-        display: block;
-        width: min(50vw, 600px);
-        position: absolute;
-        right: 3.5rem;
-    }
+  background-color: #fff;
+  transform: scaleX(-1);
+  border-radius: 50px;
+  width: min(100%, 580px);
+  margin: 1rem auto;
+  @media (min-width: 768px) {
+    display: block;
+    width: min(70vw, 600px);
+    position: absolute;
+    right: 3.5rem;
+  }
 `;
