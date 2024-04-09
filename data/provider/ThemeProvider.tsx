8225926356b9de17aa19cf51413e3b83d70aefb4
GustavoMont <lgmluisgm@gmail.com>
2024-04-09 19:17:52 -0300

    removes audio

--- data/provider/ThemeProvider.tsx
+++ data/provider/ThemeProvider.tsx
@@ -4,8 +4,6 @@ import { Themes, ThemesTokens } from "src/models/theme.tokens";
 import { lightTheme } from "src/styles/theme/light.theme";
 import { mainTheme } from "src/styles/theme/main.theme";
 import { ThemeProvider as StyledProvider } from "styled-components";
-import useSound from "use-sound";
-import darkThemeSound from "public/sounds/dark-theme-sound-theme.mp3";
 
 export const ThemeProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const [activeTheme, setActiveTheme] = useState<ThemesTokens>("main");
@@ -13,19 +11,8 @@ export const ThemeProvider: React.FC<PropsWithChildren> = ({ children }) => {
     light: lightTheme,
     main: mainTheme,
   };
-  const [play] = useSound(darkThemeSound);
-
-  const selectMainTheme = () => {
-    play({});
-  };
 
   const handleChangeTheme = (theme: ThemesTokens) => {
-    const themeAction: Record<ThemesTokens, (() => void) | undefined> = {
-      main: selectMainTheme,
-      light: undefined,
-    };
-    const onSelectTheme = themeAction[theme];
-    onSelectTheme?.();
     setActiveTheme(theme);
   };
 
