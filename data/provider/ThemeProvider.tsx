4bea8fa1dfd0365b81da81733d2675b3eb17861c
GustavoMont <lgmluisgm@gmail.com>
2024-04-09 20:19:52 -0300

    gets user theme preferences

--- data/provider/ThemeProvider.tsx
+++ data/provider/ThemeProvider.tsx
@@ -1,11 +1,13 @@
 import { ThemeContext } from "data/context/ThemeContext";
-import { PropsWithChildren, useState } from "react";
+import { useThemeDetector } from "data/hooks/useThemeDetector";
+import { PropsWithChildren, useEffect, useState } from "react";
 import { Themes, ThemesTokens } from "src/models/theme.tokens";
 import { lightTheme } from "src/styles/theme/light.theme";
 import { mainTheme } from "src/styles/theme/main.theme";
 import { ThemeProvider as StyledProvider } from "styled-components";
 
 export const ThemeProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const isDarkTheme = useThemeDetector();
   const [activeTheme, setActiveTheme] = useState<ThemesTokens>("main");
   const themes: Themes = {
     light: lightTheme,
@@ -15,6 +17,14 @@ export const ThemeProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const handleChangeTheme = (theme: ThemesTokens) => {
     setActiveTheme(theme);
   };
+
+  useEffect(() => {
+    setActiveTheme(isDarkTheme ? "main" : "light");
+
+    return () => {
+      setActiveTheme("main");
+    };
+  }, [isDarkTheme]);
 
   return (
     <ThemeContext.Provider
